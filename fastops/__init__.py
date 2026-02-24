dfb0e736c2f4848a705c2d912819a997911ee120
71293 <karthikrajgopal.laxminaarayanan@bain.com>
2026-02-25 10:25:36 +1300

    fastops preview

---
+++ fastops/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+from .core import *
+from .vps import *
+from .compose import *
+from .apps import *
+from .cloudflare import *
+from .multipass import *
+from .caddy import *
